426b519052774c3d0799acca48f62a123e1e00fe
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-04-23 10:58:56 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ class App extends React.Component {
             </p>
             <small>Digital Art and collectibles</small>
           </div>
-          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='1200px' height='800px' frameBorder='0' allowFullScreen></iframe>
+          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} frameBorder='0' allowFullScreen></iframe>
         </main>
         
       </div>
