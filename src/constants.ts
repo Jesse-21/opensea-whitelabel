771bac3d011030344ef33d8f38e6e80872bb1ab6
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-04-23 11:12:43 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ASSET_URL =
-  "https://opensea.io/collection/jesse21-private-nft-factory";
+  "https://opensea.io/collection/jesse21-private-nft-factory?Embed=true";
 
 export enum EmbedTheme {
   Default = "True",
