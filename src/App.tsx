f8a1c28eb46c12690c9aa318f0d101548baa166c
Jesse21 <98306896+Jesse-21@users.noreply.github.com>
2022-04-23 21:05:59 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,7 @@ class App extends React.Component {
           <i className="material-icons right">menu</i>
         </header>
         <main className="App-main">
-          <div className="App-hero">
-            <p>
-              Of1 NFT
-            </p>
-            <small>Digital Art and collectibles</small>
-          </div>
+<Div>
           <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='100%' height='600px' frameBorder='0' allowFullScreen></iframe>
         </main>
         
